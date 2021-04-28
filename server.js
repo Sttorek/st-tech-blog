30bfca53f6ecedde9d37ae58752e61bd11224c10
Seth Torek <seth.torek@gmail.com>
2021-04-27 20:20:25 -0400

    edited db

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set Handlebars as the default template engine.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(require('./controllers/dish-routes'));
